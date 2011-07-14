97e9814b7fd2877c8f85cc82fe46daa9138661d3
AlexisWilke <alexis@m2osw.com>
2011-07-14 14:55:28 -0700

    Issue [#1216746] by dkinzer: Addresses province field does not maintain attributes after ajax call.

--- addresses.js
+++ addresses.js
@@ -18,20 +18,29 @@ Drupal.behaviors.addresses = function(context) {
   function performProvinceAjax(countryElement) {
     // Country field's related province element
     var provinceElement=$(countryElement).parent().siblings().children('.addresses-province-field');
-
+    var attributes={};
+    // Iterates over the element attributes to create an object of attributes to pass in the ajax call.
+    if(provinceElement.length){
+      $.each(provinceElement[0].attributes,function(index,attr){
+        if(!(attr.name in{'type':1,'value':1,'size':1,'id':1,'name':1,'class':1})){
+          attributes[attr.name]=attr.value;
+        }
+      });
+    }
     $.ajax({
       type:'GET',
       url:Drupal.settings.basePath,
       success:updateProvinceField,
       dataType:'json',
-      data: {
+      data:{
         q:'addresses/province_ajax',
         country:$(countryElement).val(),
         field_id:provinceElement.attr('id'),
         field_name:provinceElement.attr('name'),
         passback:provinceElement.parent().attr('id'),
         province:provinceElement.val(),
-        language:Drupal.settings.addresses.language
+        language:Drupal.settings.addresses.language,
+        'attributes':JSON.stringify(attributes)
       }
     });
   }
